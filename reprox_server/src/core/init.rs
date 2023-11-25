bfadd3f632d9a3d0331e9a1695dfd807d10f878a
Umit Aksoylu <umitaksoylu98@gmail.com>
2023-11-25 16:53:47 +0300

    Project hierarchy implemented

--- reprox_server/src/core/init.rs
+++ reprox_server/src/core/init.rs
@@ -1,13 +1,12 @@
 use hyper::{
     service::{make_service_fn, service_fn},
-    Body, Request, Response, Server, Client, header, Uri,
+    Body, Request, Response, Server, Client, header,
 };
 use tokio::sync::Mutex;
 use std::{net::SocketAddr, collections::HashMap, sync::Arc};
 
-use crate::utils::parse_ip_address::parse_ip_address;
+use crate::{utils::parse_ip_address::parse_ip_address, api_controller::API_ROUTER};
 
-use super::static_controllers::STATIC_ROUTER;
 use hyper::client::HttpConnector;
 
 
@@ -17,23 +16,6 @@ pub struct HttpServer {
     port: u16,
     socket: SocketAddr,
     routes: HashMap<String, String>
-}
-
-// Gelen isteği yöneten fonksiyon
-async fn handle_requestfn(req: Request<hyper::Body>) -> Result<Response<hyper::Body>, hyper::Error> {
-    let target_uri = "http://example.com"; // Hedef sunucunun adresi
-    let client = Client::new();
-
-    // Yeni istek oluştur
-    let (mut parts, body) = req.into_parts();
-    let uri = format!("{}{}", target_uri, parts.uri);
-    parts.uri = uri.parse().unwrap(); // unwrap: Örnek amaçlı, hata yönetimi yapılmamıştır
-
-    // Hedef sunucuya isteği gönder
-    let proxy_req = Request::from_parts(parts, body);
-
-    let res = client.request(proxy_req).await.unwrap();
-    Ok(res)
 }
 
 impl HttpServer {
@@ -92,9 +74,9 @@ impl HttpServer {
 
         if request_host == self.ip_address.clone()
         {
-            if STATIC_ROUTER.contains_key(request_path.as_str())
+            if API_ROUTER.contains_key(request_path.as_str())
             {
-                let controller = STATIC_ROUTER.get(request_path.as_str()).unwrap();
+                let controller = API_ROUTER.get(request_path.as_str()).unwrap();
                 return controller(req);
             }
 
@@ -109,7 +91,7 @@ impl HttpServer {
 
             if endpoint_to_navigate == &default_url
             {
-                let controller = STATIC_ROUTER.get("/echo").unwrap();
+                let controller = API_ROUTER.get("/echo").unwrap();
                 return controller(req);
             }
 
@@ -125,24 +107,20 @@ impl HttpServer {
     {
         let original_uri = req.uri().clone();
     
-        // Create a new URI with the updated host and port
         let mut new_uri = format!("http://{}{}", endpoint_to_navigate, original_uri.path());
         if let Some(query) = original_uri.query() {
             new_uri.push('?');
             new_uri.push_str(query);
         }
     
-        // Create a new request with the updated URI
         let (mut parts, body) = req.into_parts();
         parts.uri = new_uri.parse().unwrap();
     
         let new_request = Request::from_parts(parts, body);
     
-        // Create a Hyper client to make the modified request
         let http = HttpConnector::new();
         let client: Client<HttpConnector> = Client::builder().build(http);
     
-        // Send the modified request and get the response
         let response = client.request(new_request).await?;
     
         Ok(response)
