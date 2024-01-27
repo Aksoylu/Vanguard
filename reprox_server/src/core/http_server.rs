defac520723926ef47b96082b86b368d5fc8dd1e
Aksoylu <umitaksoylu98@gmail.com>
2024-01-27 13:25:23 +0300

    Cargo formatting

--- reprox_server/src/core/http_server.rs
+++ reprox_server/src/core/http_server.rs
@@ -1,33 +1,33 @@
 use hyper::{
+    header,
     service::{make_service_fn, service_fn},
-    Body, Request, Response, Server, Client, header,
+    Body, Client, Request, Response, Server,
 };
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 use tokio::sync::Mutex;
-use std::{net::SocketAddr, collections::HashMap, sync::Arc};
 
 use crate::utils::parse_ip_address::parse_ip_address;
 
 use hyper::client::HttpConnector;
-
 
 #[derive(Debug, Clone)]
 pub struct HttpServer {
     ip_address: String,
     port: u16,
     socket: SocketAddr,
-    routes: HashMap<String, String>
+    routes: HashMap<String, String>,
 }
 
 impl HttpServer {
-    pub fn singleton(ip_address: &String, port: &u16, routes: HashMap<String, String>) -> Self {
+    pub fn singleton(ip_address: String, port: u16, routes: HashMap<String, String>) -> Self {
         let parsed_ip_address = parse_ip_address(ip_address.clone());
         let parsed_port = port.clone();
 
         let socket = SocketAddr::from((parsed_ip_address, parsed_port));
 
         Self {
-            ip_address: ip_address.clone(),
-            port: port.clone(),
+            ip_address,
+            port,
             socket,
             routes,
         }
@@ -47,7 +47,9 @@ impl HttpServer {
                         let data = locked_data.lock().await; // Await lock acquisition
                         match data.handle_request(req).await {
                             Ok(response) => Ok::<_, hyper::Error>(response),
-                            Err(_) => Ok::<_, hyper::Error>(Response::new(Body::from("Error processing request"))),
+                            Err(_) => Ok::<_, hyper::Error>(Response::new(Body::from(
+                                "Error processing request",
+                            ))),
                         }
                     };
                     fut
@@ -64,51 +66,54 @@ impl HttpServer {
     }
 
     async fn handle_request(&self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-        let request_host = req.headers().get(header::HOST)
+        let request_host = req
+            .headers()
+            .get(header::HOST)
             .and_then(|host| host.to_str().ok())
             .map_or_else(|| "/".to_string(), |host_value| host_value.to_string());
 
-        if !self.routes.contains_key(&request_host)
-        {
-            let response = Response::new(Body::from("Requested Reprox redirection URL not found..."));
-            return Ok(response)
+        if !self.routes.contains_key(&request_host) {
+            let response =
+                Response::new(Body::from("Requested Reprox redirection URL not found..."));
+            return Ok(response);
         }
 
-        let default_url = "".to_owned();           
+        let default_url = "".to_owned();
         let endpoint_to_navigate = self.routes.get(&request_host).unwrap_or(&default_url);
 
-        if endpoint_to_navigate == &default_url
-        {
-            let response = Response::new(Body::from("Requested Reprox redirection URL not found..."));
-            return Ok(response)
+        if endpoint_to_navigate == &default_url {
+            let response =
+                Response::new(Body::from("Requested Reprox redirection URL not found..."));
+            return Ok(response);
         }
 
         let response = self.navigate_url(endpoint_to_navigate, req).await;
         return response;
     }
 
-    async fn navigate_url(&self, endpoint_to_navigate: &String, req: Request<Body>) -> Result<Response<Body>, hyper::Error>
-    {
+    async fn navigate_url(
+        &self,
+        endpoint_to_navigate: &String,
+        req: Request<Body>,
+    ) -> Result<Response<Body>, hyper::Error> {
         let original_uri = req.uri().clone();
-    
+
         let mut new_uri = format!("http://{}{}", endpoint_to_navigate, original_uri.path());
         if let Some(query) = original_uri.query() {
             new_uri.push('?');
             new_uri.push_str(query);
         }
-    
+
         let (mut parts, body) = req.into_parts();
         parts.uri = new_uri.parse().unwrap();
-    
+
         let new_request = Request::from_parts(parts, body);
-    
+
         let http = HttpConnector::new();
         let client: Client<HttpConnector> = Client::builder().build(http);
-    
+
         let response = client.request(new_request).await?;
-    
+
         Ok(response)
     }
-
 }
-
--- reprox_server/src/utils/mod.rs
+++ reprox_server/src/utils/mod.rs
@@ -1,4 +1,4 @@
 pub mod config;
-pub mod str_to_i8;
 pub mod parse_ip_address;
 pub mod routing;
+pub mod str_to_i8;
