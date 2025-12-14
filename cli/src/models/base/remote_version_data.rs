74fdcc699639f3080c63d505f08a7727876eb79d
Ümit Aksoylu <umitaksoylu@Umit-MacBook-Pro.local>
2025-12-14 19:29:39 +0300

    feat:remote version control implementation

--- cli/src/models/base/remote_version_data.rs
+++ cli/src/models/base/remote_version_data.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct RemoteVersionData{
     #[serde(rename = "VERSION_NUMBER")]
-    pub version_number: String,
+    pub version_number: f64,
     #[serde(rename = "VERSION_NAME")]
     pub version_name: String
 }
