e54ae626d9c72b769c10bdb0f3c545bc84ecb3bb
Ümit Aksoylu <umitaksoylu@Umit-MacBook-Pro.local>
2026-02-08 01:43:10 +0300

    feat: content length reading error handled

--- engine/src/utils/http_utility.rs
+++ engine/src/utils/http_utility.rs
@@ -3,8 +3,10 @@ use hyper::{Body, Request};
 use crate::common::errors::hyper_error::HyperError;
 
 pub fn get_content_length(req: &Request<Body>) -> Result<u64, HyperError> {
-    let header_value = req.headers().get(hyper::header::CONTENT_LENGTH)
-        .ok_or_else(|| HyperError::from("Content-Length header missing"))?;
+    let header_value = match req.headers().get(hyper::header::CONTENT_LENGTH) {
+        Some(v) => v,
+        None => return Ok(0),
+    };
 
     let content_length_str = header_value.to_str()
         .map_err(|_| HyperError::from("Invalid Content-Length header"))?;
